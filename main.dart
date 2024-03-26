aa26beb76ff47bd17dad518336000104aba5d42c
khomotso gojela <motsoak49@gmail.com>
2024-03-26 10:14:46 +0200

    first commit

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main() {
+  int num = 11;
+
+  if (num > 10) {
+    print("Your number is greater than 10");
+  } else if (num < 10) {
+    print("Your number is less than 10");
+  } else if (num == 10) {
+    print("Your number is equal to 10");
+  }
+}
